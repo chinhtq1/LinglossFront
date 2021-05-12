251e271a4e71cff11153951b2b565b2c0fa782b1
Hanna <surmach.anna@mail.ru>
2021-05-13 00:51:00 +0300

    Logout when authorized

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocalStorageService} from '../../services/local-storage.service';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  user: User = null;
+
+  constructor(private lss: LocalStorageService) {
+  }
+
+  ngOnInit(): void {
+    this.user = this.lss.getUser();
+  }
+
+  logout(): void {
+    this.lss.clearUser();
+    window.location.reload();
+    this.ngOnInit();
+  }
 }
