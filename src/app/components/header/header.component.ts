b2bcb28cedbfe9c79f0657b469c619a8b07a3aa0
Hanna <surmach.anna@mail.ru>
2021-05-13 15:59:55 +0300

    Remove sider from test version

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {faTelegramPlane} from '@fortawesome/free-brands-svg-icons';
-import {faInfo, faGlobeAmericas} from '@fortawesome/free-solid-svg-icons';
+import {faInfo, faGlobeAmericas, faBook} from '@fortawesome/free-solid-svg-icons';
 import {faUser} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
@@ -13,6 +13,7 @@ export class HeaderComponent {
   faGlobe = faGlobeAmericas;
   faInfo = faInfo;
   faUser = faUser;
+  faBook = faBook;
 
   isModalVisible = false;
 
