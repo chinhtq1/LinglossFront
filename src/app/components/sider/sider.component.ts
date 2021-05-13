a2281deadd88a4adcd42cc31dfb6f650f308b703
Hanna <surmach.anna@mail.ru>
2021-05-13 13:57:41 +0300

    Fix console error in Sider

--- src/app/components/sider/sider.component.ts
+++ src/app/components/sider/sider.component.ts
@@ -1,35 +1,19 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import { Discipline } from '../../models/discipline';
 import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
-import { DisciplineService } from '../../services/discipline.service';
 
 @Component({
   selector: 'app-sider',
   templateUrl: './sider.component.html',
   styleUrls: ['./sider.component.scss']
 })
-export class SiderComponent implements OnInit {
-  disciplines: Discipline[] = [];
+export class SiderComponent {
 
   @Output()
   closeSider = new EventEmitter<boolean>();
 
-  constructor(public router: Router,
-              private userService: UserService,
-              private disciplineService: DisciplineService) {
-    // tslint:disable-next-line:only-arrow-functions
-    this.router.routeReuseStrategy.shouldReuseRoute = function() {
-      return false;
-    };
-  }
-
-  ngOnInit(): void {
-    this.userService.getUser('1').subscribe(data => {
-      data.disciplines.forEach(id => this.disciplineService.getDiscipline(id).subscribe(discipline => {
-        this.disciplines.push(discipline);
-      }));
-    }); // TODO get user id from localstorage
+  constructor(public router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   navigateToAllDisciplines(): void {
