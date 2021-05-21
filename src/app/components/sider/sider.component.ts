87fc04d6f0e70604ab05978144020eec89daa6ef
Hanna <surmach.anna@mail.ru>
2021-05-21 21:59:41 +0300

    Fix console error in Sider

--- src/app/components/sider/sider.component.ts
+++ src/app/components/sider/sider.component.ts
@@ -1,40 +1,19 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import { Discipline } from '../../models/discipline';
 import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
-import { DisciplineService } from '../../services/discipline.service';
-import {LocalStorageService} from '../../services/local-storage.service';
-import {User} from '../../models/user';
 
 @Component({
   selector: 'app-sider',
   templateUrl: './sider.component.html',
   styleUrls: ['./sider.component.scss']
 })
-export class SiderComponent implements OnInit {
-  disciplines: Discipline[] = [];
-  user: User = {} as User;
+export class SiderComponent {
 
   @Output()
   closeSider = new EventEmitter<boolean>();
 
-  constructor(public router: Router,
-              private userService: UserService,
-              private disciplineService: DisciplineService,
-              private lss: LocalStorageService) {
-    this.router.routeReuseStrategy.shouldReuseRoute = (): boolean => false;
-  }
-
-  ngOnInit(): void {
-    this.user = !!this.lss.getUser() ? this.lss.getUser() : this.user;
-    if (this.user && this.user.disciplines && this.user.disciplines.length) {
-      this.user.disciplines.forEach(disciplineId => {
-        this.disciplineService.getDiscipline(disciplineId).subscribe(discipline => {
-            this.disciplines.push(discipline);
-          }
-        );
-      });
-    }
+  constructor(public router: Router) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
   navigateToAllDisciplines(): void {
